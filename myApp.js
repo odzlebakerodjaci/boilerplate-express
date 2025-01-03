0b098baa50688efe9f815e11f7391e5b9345d383
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-03 16:24:14 +0100

    adding string on root route

--- myApp.js
+++ myApp.js
@@ -4,8 +4,12 @@ let app = express();
 console.log("Hello World")
 
 
-
-
+//app.METHOD(PATH, HANDLER)
+//HANDLER = function(req,res){res.send("string")}
+//Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path. 
+app.get('/', (req,res)=>{
+    res.send("Hello Express");
+});
 
 
 
