b852180842c4b16214d3e22d956d2abbdab51448
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-03 17:10:30 +0100

    uppercase message

--- myApp.js
+++ myApp.js
@@ -5,9 +5,7 @@ require('dotenv').config()
 console.log("Hello World")
 
 
-//app.METHOD(PATH, HANDLER)
-//HANDLER = function(req,res){res.send("string")}
-//Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path. 
+
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
@@ -15,11 +13,13 @@ app.get('/', (req,res)=>{
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req,res)=>{
+    let message = "Hello json"; 
+
     if (process.env.MESSAGE_STYLE === "uppercase") {
         message = message.toUpperCase();
       }
-      
-    res.json({"message": "Hello json"})
+
+    res.json({"message": message})
 });
 
 
