cd23078943613ffcfc79b694de93f368a41c1e82
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-03 17:15:35 +0100

    uppercase

--- myApp.js
+++ myApp.js
@@ -2,49 +2,23 @@ let express = require('express');
 let app = express();
 require('dotenv').config()
 
-console.log("Hello World")
+console.log("Hello World");
 
-
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + "/views/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req,res)=>{
-    let message = "Hello json"; 
+app.get('/json', (req, res) => {
+  let message = "Hello json"; // Default message
 
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        message = message.toUpperCase();
-      }
+  // If the MESSAGE_STYLE environment variable is set to "uppercase"
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase(); // Convert message to uppercase
+  }
 
-    res.json({"message": message})
+  res.json({ "message": message }); // Send the message as a JSON response
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
