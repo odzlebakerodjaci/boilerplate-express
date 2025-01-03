52aeef61169ccee5ffac8915a202fadc5a7f668e
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-03 16:38:42 +0100

    adding static files

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use(__dirname + '/public', express.static());
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 
