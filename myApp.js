70ab701b7f5cb17bc26c2b4246c7da3b22f78bdf
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-04 16:07:36 +0100

    final look

--- myApp.js
+++ myApp.js
@@ -1,8 +1,38 @@
 require('dotenv').config();
+let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next(); 
+});
+app.use(bodyParser.urlencoded({ extended: false }));  
+
+app.route('/name')
+  // GET request handler (for form data)
+  .get((req, res) => {
+    res.sendFile(__dirname + "/views/index.html");  // Ensure your form is in this file
+  })
+  // POST request handler (to handle form submission)
+  .post((req, res) => {
+    const { first, last } = req.body; // Retrieve the data from the body
+    if (first && last) {
+      res.json({ name: `${first} ${last}` });
+    } else {
+      res.status(400).json({ error: "Both 'first' and 'last' body parameters are required." });
+    }
+  });
+
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString(); 
+  next();  
+}, function(req, res) {
+  res.json({ time: req.time });  
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
@@ -21,6 +51,36 @@ app.get('/json', (req, res) => {
   
     res.json({ "message": message }); 
   });
+
+  app.get('/:word/echo', (req, res) => {
+    const word = req.params.word; 
+    res.json({ echo: word });     
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
